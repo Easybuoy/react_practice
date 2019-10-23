4f04dff53a6829a1823274650de761d8c55ddc61
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-10-23 23:23:10 +0100

    implement counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count+1);
+  };
   return (
     <div className="App">
-     
+      <button onClick={incrementCount}>Click {count} times</button>
     </div>
   );
 }
