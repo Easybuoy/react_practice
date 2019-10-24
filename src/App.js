c67eff564b10ce074a54f77353a37e618938a59c
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-10-24 16:11:02 +0100

    practice more

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import './App.css';
-import Counter from './Counter';
+// import React from 'react';
+// import './App.css';
+// import Counter from './Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <Counter />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { Component } from 'react';
+import Counter from './Counter';
+import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
+
+export class App extends Component {
+  componentDidMount() {
+    console.log('a');
+    this.interval = setTimeout(() => console.log('wawues'), 1000);
+  }
+
+  componentWillUnmount() {
+    console.log('aa');
+  }
+
+  componentWillMount() {
+    console.log('willmount');
+    clearInterval(this.interval);
+  }
+  render() {
+    return (
+      <Router>
+        <Link to="/me">Click here</Link>
+        <Route exact path="/" component={Counter} />
+        <Route exact path="/me" component={Counter} />
+      </Router>
+    );
+  }
 }
 
 export default App;
